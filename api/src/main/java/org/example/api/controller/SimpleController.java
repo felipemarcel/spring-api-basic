5d85052a33a55b34ac247582289dafd62680e4bc
Felipe Marcel <felipe.queiroz@seplag.ce.gov.br>
2024-03-08 09:28:00 -0300

    Ajustando retorno do primeiro endpoint

--- api/src/main/java/org/example/api/controller/SimpleController.java
+++ api/src/main/java/org/example/api/controller/SimpleController.java
@@ -1,15 +1,18 @@
 package org.example.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping
 public class SimpleController {
 
     @GetMapping("/api-version")
-    public String getApiVersion() {
-        return "0.0.1";
+    public ResponseEntity<String> getApiVersion() {
+        return ok("0.0.1");
     }
 }
